18cedf73d85a62dc4d5c0373fb634099fc50f02a
pranavi.jampana <pranavi.jampana@PranaviJampana.covalensedigital.net>
2021-11-17 11:36:01 +0530

    Commit 2:Created another Class

---
+++ SpringDemo_02/src/com/cov/app/Teat_01.java
@@ -0,0 +1,11 @@
+package com.cov.app;
+
+public class Teat_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Student Application");
+
+	}
+
+}
