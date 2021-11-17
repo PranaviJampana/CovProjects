7a108370aeca423ad52de15612d1d1a41950d639
pranavi.jampana <pranavi.jampana@PranaviJampana.covalensedigital.net>
2021-11-17 11:37:20 +0530

    Commit 2:Modified the previously defined class

--- SpringDemo_02/src/com/cov/app/Teat_01.java
+++ SpringDemo_02/src/com/cov/app/Teat_01.java
@@ -5,6 +5,7 @@ public class Teat_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Student Application");
+		System.err.println("Final Application");
 
 	}
 
